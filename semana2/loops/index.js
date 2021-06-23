6f3f701c04ee40f6177655982fcc76b2641cfaf4
Beatriz <anabdemorais@gmail.com>
2021-06-23 14:53:08 -0300

    exercicio loops

---
+++ semana2/loops/index.js
@@ -0,0 +1,32 @@
+//>>>>>>>>>>>Interpretação de código
+
+//1. será impresso 10
+//2. 10, 11, 12, 15.
+//3.*
+//**
+//***
+//**** 
+
+//>>>>>>>>>>> Escrita de código
+
+//1.
+const checaBichinhos = () => {
+  let bichinhosDoUser = []
+}
+let quantidadeBichinhos = Number(prompt('Quantos bichinhos você tem?'))
+
+if (quantidadeBichinhos === 0) {
+  console.log('Que pena! Mas você pode adotar um pet!')
+} else {
+  while (bichinhosDoUser < quantidadeBichinhos) {
+    let nomeBichinho = prompt('Digite o nome do seu bichinho!')
+    nomeBichinho.push(bichinhosDoUser)
+  }
+
+  console.log(bichinhosDoUser)
+}
+
+//2.
+const arrayOriginal = [10, 20, 30, 40, 50, 60]
+for (int = 0; i < arrayOriginal.length; i++ / 0) console.log(arrayOriginal)
+*/
