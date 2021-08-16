ea929fd9286430cfcd09d0ed9518dc847849d83e
Beatriz <anabdemorais@gmail.com>
2021-08-16 16:53:30 -0300

    git hub pediu pra fazer commit pra mudar de branch

--- semana 8/AstroMatch-Ana/astromatch/src/Components/Buttons.js
+++ semana 8/AstroMatch-Ana/astromatch/src/Components/Buttons.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './Buttons.css';
-import CloseIcon from "@material-ui/icons/Close";
-import StarRateIcon from "@material-ui/icons/StarRateIcon";
-import FavoriteIcon from "@material-ui/icons/FavoriteIcon";
+//import CloseIcon from "@material-ui/icons/Close";
+//import StarRateIcon from "@material-ui/icons/StarRateIcon";
+//import FavoriteIcon from "@material-ui/icons/FavoriteIcon";
 
 
 const Buttons = () => {
   return(
     <div className="Buttons">
-      <CloseIcon />
-      <StarRateIcon />
-      <FavoriteIcon />
+      teste icons aqui
     </div>
   )
 }
