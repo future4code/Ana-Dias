17f6b427eda887133b924795cc3d287d2a2728b0
Beatriz <anabdemorais@gmail.com>
2021-08-22 23:32:48 -0300

    trabalho

--- semana 10/labeX/labex-ana/src/App.js
+++ semana 10/labeX/labex-ana/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from './Pages/HomePage/HomePage';
+import CreateTripPage from './Pages/ListTripsPage/CreateTripPage';
 import ListTripsPage from './Pages/ListTripsPage/ListTripsPage';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
         <HomePage/> 
        <ListTripsPage />
+       <CreateTripPage />
     </div>
   );
 }
--- semana 10/labeX/labex-ana/src/Pages/ListTripsPage/ListTripsPage.js
+++ semana 10/labeX/labex-ana/src/Pages/ListTripsPage/ListTripsPage.js
@@ -2,18 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ListTripsPageContainer = styled.div`
-  background: light blue;
+  background: #6959CD;
+  margin: 10px;
+  
 ` 
+
 
 function ListTripsPage() {
   return (
     <ListTripsPageContainer>
-    <select>teste</select>
-    <ol>
-  <li>Viagem</li>
-  <li>Viagem</li>
-  <li>Viagem</li>
-    </ol>
+   aaaaaaaaaa
     </ListTripsPageContainer>
   );
 }
